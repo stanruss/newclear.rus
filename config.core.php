f9f3845a5b9f1b16260fe6c97c82b7410d0c78c3
stanruss <sp.tver@rusalut.ru>
2018-10-25 13:29:13 +0300

    update

--- config.core.php
+++ config.core.php
@@ -4,6 +4,6 @@
  * Add customizations to the $config_options array in `core/config/config.inc.php`.
  *
  */
-define('MODX_CORE_PATH', 'C:/OpenServer/domains/newclear.rus/core/');
+define('MODX_CORE_PATH', 'C:/OSPanel/domains/newclear.rus/core/');
 define('MODX_CONFIG_KEY', 'config');
 ?>
--- core/config/config.inc.php
+++ core/config/config.inc.php
@@ -5,7 +5,7 @@
 $database_type = 'mysql';
 $database_server = 'localhost';
 $database_user = 'root';
-$database_password = '';
+$database_password = '818791';
 $database_connection_charset = 'utf8';
 $dbase = 'newclear';
 $table_prefix = 'new_';
@@ -23,27 +23,27 @@ $https_port = '443';
 $uuid = 'd00b02c7-6588-48d8-98bf-cfa75afb52b7';
 
 if (!defined('MODX_CORE_PATH')) {
-    $modx_core_path= 'C:/OpenServer/domains/newclear.rus/core/';
+    $modx_core_path= 'C:/OSPanel/domains/newclear.rus/core/';
     define('MODX_CORE_PATH', $modx_core_path);
 }
 if (!defined('MODX_PROCESSORS_PATH')) {
-    $modx_processors_path= 'C:/OpenServer/domains/newclear.rus/core/model/modx/processors/';
+    $modx_processors_path= 'C:/OSPanel/domains/newclear.rus/core/model/modx/processors/';
     define('MODX_PROCESSORS_PATH', $modx_processors_path);
 }
 if (!defined('MODX_CONNECTORS_PATH')) {
-    $modx_connectors_path= 'C:/OpenServer/domains/newclear.rus/connectors/';
+    $modx_connectors_path= 'C:/OSPanel/domains/newclear.rus/connectors/';
     $modx_connectors_url= '/connectors/';
     define('MODX_CONNECTORS_PATH', $modx_connectors_path);
     define('MODX_CONNECTORS_URL', $modx_connectors_url);
 }
 if (!defined('MODX_MANAGER_PATH')) {
-    $modx_manager_path= 'C:/OpenServer/domains/newclear.rus/manager/';
+    $modx_manager_path= 'C:/OSPanel/domains/newclear.rus/manager/';
     $modx_manager_url= '/manager/';
     define('MODX_MANAGER_PATH', $modx_manager_path);
     define('MODX_MANAGER_URL', $modx_manager_url);
 }
 if (!defined('MODX_BASE_PATH')) {
-    $modx_base_path= 'C:/OpenServer/domains/newclear.rus/';
+    $modx_base_path= 'C:/OSPanel/domains/newclear.rus/';
     $modx_base_url= '/';
     define('MODX_BASE_PATH', $modx_base_path);
     define('MODX_BASE_URL', $modx_base_url);
@@ -75,7 +75,7 @@ if (!defined('MODX_SITE_URL')) {
     define('MODX_SITE_URL', $site_url);
 }
 if (!defined('MODX_ASSETS_PATH')) {
-    $modx_assets_path= 'C:/OpenServer/domains/newclear.rus/assets/';
+    $modx_assets_path= 'C:/OSPanel/domains/newclear.rus/assets/';
     $modx_assets_url= '/assets/';
     define('MODX_ASSETS_PATH', $modx_assets_path);
     define('MODX_ASSETS_URL', $modx_assets_url);
